862c2bafe77b7f3f7859baf50fd848f3a114e95b
Vanshika <jainvanshika0411@gmail.com>
2026-03-02 15:29:33 +0530

    Fixed Clerk protect middleware 401 issue

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -8,6 +8,7 @@ export const protect = async (req, res, next) => {
       return res.status(401).json({ success: false, message: "Not authenticated" });
     }
 
+    // const user = await User.findById(userId);
     const user = await User.findById(userId);
     if (!user) {
       return res.status(401).json({ success: false, message: "User not found" });
